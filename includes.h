55b5eeb587e389466c299df9ec3de537fb67a8d3
mxheyyy <mahe.cauchon@gmail.com>
2022-12-21 14:39:23 +0100

    push includes

---
+++ includes.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_FILE
+#define HEADERFILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <math.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
+
+#endif
