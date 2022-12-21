c6897fa95d9afc470292240b04a8991d36c51c33
Frexom <hugom2003@sfr.fr>
2022-12-21 23:24:23 +0100

    Améliorations -Ajout de commentaires -Suppression des pipes nommées sur disque après utilisation

--- files/client.c
+++ files/client.c
@@ -8,70 +8,79 @@ typedef struct {
 int main(int argc, char *argv[]) {
 
 
-	// Arguments
+	// Initialisation des variables
 	char moyenne[10];
 	key_t cle;
 	int msgId;
 	MSG requete;
 
-	// Vérif nombre d'arguments
-	if(argc == 2) {
-		if(atoi(argv[1]) > 0 && atoi(argv[1]) < 6) {
+	// Vérification du nombre et de la validité des arguments
+	if(argc >= 2 && atoi(argv[1]) > 0 && atoi(argv[1]) < 6) {
 
-			// Création du message en mémoire
-
-			cle = ftok("/etc/passwd",0);
-			if(cle == -1) {
-				perror("ftok");
-				return EXIT_FAILURE;
-			}
-
-			msgId = msgget(cle, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
-			if (msgId == -1) {
-				msgId = msgget(cle, IPC_EXCL);
-				if(msgId == -1) {
-					perror("msgget");
-					return EXIT_FAILURE;
-				}
-				printf("La MSG existait déjà, identificateur : %d\n", msgId);
-			}
-
-			// Remplissage de la struct
-
-			requete.matiere = atoi(argv[1]);
-			sprintf(requete.pid, "%d", getpid());
-
-			// Envoi du message dans la message queue
-			if((msgsnd(msgId, &requete, sizeof(MSG) - sizeof(int), 0)) == -1) {
-				perror("msgsnd");
-				return EXIT_FAILURE;
-			}
-			else{
-				printf("MSG créé avec la clé %x \n", cle);
-				printf("Identificateur de la MSG : %d\n", msgId);
-				printf("Matière demandée : %d\n", requete.matiere);
-				printf("PIDclient : %d\n", getpid());
-				printf("Envoi réussi\n\n");
-
-			}
-
-			// Pipe nommé
-			char path[] = "/tmp/";
-			strcat(path, requete.pid);
-			mkfifo(path,0666);
-			int fd = open(path, O_RDONLY);
-			read(fd, moyenne, 10);
-			close(fd);
-
-			float valeur = atof(moyenne);
-			printf("Moyenne reçue : %f\n", valeur);
-
-		} else {
+		//Optention de la clé pour la file de messages
+		cle = ftok("/etc/passwd",0);
+		if(cle == -1) {
+			perror("ftok");
 			return EXIT_FAILURE;
 		}
 
+		//Création de la file
+		msgId = msgget(cle, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+		if (msgId == -1) {
+			msgId = msgget(cle, IPC_EXCL);
+			if(msgId == -1) {
+				perror("msgget");
+				return EXIT_FAILURE;
+			}
+			printf("La MSG existait déjà, identificateur : %d\n", msgId);
+		}
+
+		// Remplissage du contenu du message
+		requete.matiere = atoi(argv[1]);
+		sprintf(requete.pid, "%d", getpid());
+
+
+		// Envoi du message dans la message queue
+		if((msgsnd(msgId, &requete, sizeof(MSG) - sizeof(int), 0)) == -1) {
+			perror("msgsnd");
+			return EXIT_FAILURE;
+		}
+		else{
+			//Impression des logs
+			printf("MSG créé avec la clé %x \n", cle);
+			printf("Identificateur de la MSG : %d\n", msgId);
+			printf("Matière demandée : %d\n", requete.matiere);
+			printf("PIDclient : %d\n", getpid());
+			printf("Envoi réussi\n\n");
+
+		}
+
+		// Création du pipe nommé en lecture
+		char path[] = "/tmp/";
+		strcat(path, requete.pid);
+		mkfifo(path,0666);
+		int fd = open(path, O_RDONLY);
+		//Lecture
+		read(fd, moyenne, 10);
+
+		//Fermeture
+		close(fd);
+
+		//Suppression du pipe sur le disque
+		char command[30] = "rm ";
+		strcat(command, "/tmp/");
+		strcat(command, requete.pid);
+		printf("%s\n", command);
+		system(command);
+
+		//Impression
+		float valeur = atof(moyenne);
+		printf("Moyenne reçue : %f\n", valeur);
+
+
 		return EXIT_SUCCESS;
 	} else {
+		printf("Veuillez spécifier un paramètre numérique entre 1 et 5 inclus.\n");
 		return EXIT_FAILURE;
 	}
 }
--- files/server.c
+++ files/server.c
@@ -1,17 +1,19 @@
 #include "../includes.h"
 
+//Structure du message dans la file de messages
 struct MSG {
     int numMatiere;
     char pid[10];
 }MSG;
 
+//Structure représentant une matière
 typedef struct Matiere {
     int id;
     char nom[20];
     float moyenne;
 }Matiere;
 
-
+//Constructeur d'une Matière
 Matiere creerMatiere(int id, char* nom, float moyenne){
         Matiere m;
         m.id = id;
@@ -24,14 +26,15 @@ Matiere creerMatiere(int id, char* nom, float moyenne){
 int main(int argc, char const *argv[]) {
     printf("Démarrage du serveur\n");
 
+    //Création des matières
     Matiere moyennes[5];
-
     moyennes[0] = creerMatiere(1, "Anglais", 12.5);
     moyennes[1] = creerMatiere(2, "Mathématiques", 10.8);
     moyennes[2] = creerMatiere(3, "Informatique", 9.3);
     moyennes[3] = creerMatiere(4, "CultureG", 12.6);
     moyennes[4] = creerMatiere(5, "Physique", 11.8);
 
+    //Pour rester tout le temps actif
     while(1){
 
         //Création de la clé
@@ -43,30 +46,37 @@ int main(int argc, char const *argv[]) {
 
 
         //Obtention de la file de messages
-        int fifo = msgget(key, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
-        if(fifo == -1){
-            fifo = msgget(key, IPC_EXCL);
-            if(fifo == -1){
+        int mesQueue = msgget(key, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+        if(mesQueue == -1){
+            mesQueue = msgget(key, IPC_EXCL);
+            if(mesQueue == -1){
                 printf("Erreur avec la file\n");
                 return EXIT_FAILURE;
             }
         }
 
+
         struct MSG msg;
 
-        msgrcv(fifo, &msg, sizeof(MSG), 0, 0);
+        //Obtention du message
+        msgrcv(mesQueue, &msg, sizeof(MSG), 0, 0);
+
+
+        //Impression des logs
         printf("Côté serveur, message reçu\n");
         printf("Numéro de matière : %d\n", msg.numMatiere);
         printf("PID : %s\n", msg.pid);
+        printf("Moyenne envoyée : %f\n\n", moyennes[msg.numMatiere-1].moyenne);
 
 
-        mkfifo(msg.pid, 0666);
+        //Création du pipe nommé
         char path[20] = "/tmp/";
         strcat(path, msg.pid);
+        mkfifo(path, 0666);
         int fd = open(path, O_WRONLY);
 
+        //Écriture dans le pipe
         char moyenne[10];
-        printf("Moyenne envoyée : %f\n\n", moyennes[msg.numMatiere-1].moyenne);
         sprintf(moyenne, "%f", moyennes[msg.numMatiere-1].moyenne);
         write(fd, moyenne, strlen(moyenne)+1);
         close(fd);
